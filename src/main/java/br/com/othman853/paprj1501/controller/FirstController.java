5a2bde4d13f46e7e97e14d1a3a6fbfbb6e872b42
othman853 <anuar.yasser@gmail.com>
2015-03-04 14:46:16 -0300

    Setting up Hibernate

--- src/main/java/br/com/othman853/paprj1501/controller/FirstController.java
+++ src/main/java/br/com/othman853/paprj1501/controller/FirstController.java
@@ -1,8 +1,16 @@
 package br.com.othman853.paprj1501.controller;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.othman853.paprj1501.model.HelloHibernate;
 
 @Resource
 public class FirstController {
@@ -16,6 +24,53 @@ public class FirstController {
 	@Path("/")
 	public void first(){
 		result.include("message", "hello Vraptor");
+	}
+	
+	@Get("/hiber/{text}")
+	public void testHibernate(String text){
+	
+		
+		Configuration config = new Configuration();
+		
+		config.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/hiber_test");
+		config.setProperty("hibernate.connection.username", "padrao");
+		config.setProperty("hibernate.connection.password", "pass@word1");
+		
+		config.configure();
+		
+		Session session = config.buildSessionFactory().openSession();
+		
+		Transaction tx = session.beginTransaction();
+		session.save(new HelloHibernate(text));
+		tx.commit();	
+		
+		session.close();
+		
+		result.redirectTo(this).list();
+	}
+	
+	
+	public List<HelloHibernate> list(){
+		
+		List<HelloHibernate> list;
+		
+		Configuration config = new Configuration();
+		
+		config.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/hiber_test");
+		config.setProperty("hibernate.connection.username", "padrao");
+		config.setProperty("hibernate.connection.password", "pass@word1");
+		
+		config.configure();
+		
+		Session session = config.buildSessionFactory().openSession();
+		list = session.createCriteria(HelloHibernate.class).list();
+		session.close();
+		
+		for(HelloHibernate hib : list){
+			System.out.println(hib.getHelloText());
+		}
+		
+		return list;		
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/first/list.jsp
+++ src/main/webapp/WEB-INF/jsp/first/list.jsp
@@ -3,10 +3,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 </head>
-<body>
-
-</body>
+	<body>
+		<ul>
+				<li>Items:</li>
+			<c:forEach items="${list}" var="helloHibernate">
+				<li>Value : ${helloHibernate.helloText}</li>
+			</c:forEach>
+		</ul>
+	</body>
 </html>
